8ba177cec2d04661f5a8d13245c35e3ed4e30178
Yassine <91252807+Yassinical@users.noreply.github.com>
2025-04-10 16:19:33 +0200

    Add files via upload

---
+++ chat/script.js
@@ -0,0 +1,15 @@
+document.getElementById("btnplanning").addEventListener("click", () => {
+  document.querySelector(".phone").style.backgroundImage = "url('img/planning.png')";
+  document.querySelector("iframe").style.visibility = "hidden";
+  document.querySelector(".chatbot").style.visibility = "visible"
+});
+
+document.getElementById("btnprofile").addEventListener("click", () => {
+  document.querySelector(".chatbot").style.visibility = "hidden"
+  document.querySelector("iframe").style.visibility = "hidden";
+  document.querySelector(".phone").style.backgroundImage = "url('img/profile.png')";
+});
+
+document.querySelector(".chatbot").addEventListener("click", () => {
+  document.querySelector("iframe").style.visibility = "visible";
+});
