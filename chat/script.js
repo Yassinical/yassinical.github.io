20274914a60343be01674885ce610364bac69dc1
Yassine <91252807+Yassinical@users.noreply.github.com>
2025-04-24 16:49:04 +0200

    Update script.js

--- chat/script.js
+++ chat/script.js
@@ -1,31 +1,37 @@
-window.addEventListener('load', function() {
-  const bgImages = [
-    'img/planning.png',
-    'img/profile.png'
-  ];
+const phoneElement = document.querySelector(".phone");
+const iframeElement = document.querySelector("iframe");
+const chatbotElement = document.querySelector(".chatbot");
 
+const appImages = {
+  planning: new Image(),
+  profile: new Image()
+};
+
+window.addEventListener('load', function() {
+  appImages.planning.src = 'img/planning.png';
+  appImages.profile.src = 'img/profile.png';
   
-  bgImages.forEach(url => {
-    const img = new Image();
-    img.src = url;
-  });
+  appImages.planning.onload = function() {
+    console.log('Planning image loaded');
+  };
+  appImages.profile.onload = function() {
+    console.log('Profile image loaded');
+  };
 });
 
-
 document.getElementById("btnplanning").addEventListener("click", () => {
-  //document.querySelector(".phone").style.backgroundImage = "url('img/planning.png')";
-  document.querySelector(".phone").style.backgroundImage = bgImages[0];
-  document.querySelector("iframe").style.visibility = "hidden";
-  document.querySelector(".chatbot").style.visibility = "visible"
+  phoneElement.style.backgroundImage = `url('${appImages.planning.src}')`;
+  iframeElement.style.visibility = "hidden";
+  chatbotElement.style.visibility = "visible";
 });
 
 document.getElementById("btnprofile").addEventListener("click", () => {
-  document.querySelector(".chatbot").style.visibility = "hidden"
-  document.querySelector("iframe").style.visibility = "hidden";
-  document.querySelector(".phone").style.backgroundImage = bgImages[1];
+  chatbotElement.style.visibility = "hidden";
+  iframeElement.style.visibility = "hidden";
+  phoneElement.style.backgroundImage = `url('${appImages.profile.src}')`;
 });
 
-document.querySelector(".chatbot").addEventListener("click", () => {
-  document.querySelector("iframe").style.visibility = "visible";
-  document.querySelector(".chatbot").style.visibility = "hidden";
+chatbotElement.addEventListener("click", () => {
+  iframeElement.style.visibility = "visible";
+  chatbotElement.style.visibility = "hidden";
 });
